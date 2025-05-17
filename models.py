69e30550530b647e0a1f2ce50045c60fa677f02c
Aidarkhan <doyanbaevaidarkhan@gmail.com>
2025-05-17 08:38:34 +0500

    did db and migrate upgrade

--- models.py
+++ models.py
@@ -1,24 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate  # 👈 обязательно
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'mysecretkey'
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)  # 👈 обязательно
+migrate = Migrate(app, db)
 
-# Модель пользователя
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     posts = db.relationship('Post', backref='user', lazy=True)
 
-# Модель поста
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    image_filename = db.Column(db.String(100))  # <-- new
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
