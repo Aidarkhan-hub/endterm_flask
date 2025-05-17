7c184492970d5394ba4ec9203067d32e985b8f9f
Aidarkhan <doyanbaevaidarkhan@gmail.com>
2025-05-17 05:16:20 +0500

    Added notifications

--- models.py
+++ models.py
@@ -6,12 +6,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'mysecretkey'
 db = SQLAlchemy(app)
 
-# Таблица пользователей
+# Модель пользователя
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    posts = db.relationship('Post', backref='user', lazy=True)
 
-# Создание таблиц
-with app.app_context():
-    db.create_all()
+# Модель поста
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+# Создание базы
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
